c013a12c818ddd5381229882c1ab1a61062d4bb5
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-10-03 18:40:20 +0530

    updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://web-3-project-iwpi.onrender.com/api/auth/login", {
         email,
         password,
       });
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -37,7 +37,7 @@ const Signup = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", {
+      const res = await axios.post("https://web-3-project-iwpi.onrender.com/api/auth/signup", {
         name,
         email,
         password,
