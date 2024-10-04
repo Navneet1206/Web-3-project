4cc2a723cbecf0f3c1f6e565d7b7bad4577f2151
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-10-04 15:47:02 +0530

    Transaction.json is done

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0xEe9406957C269d8628C8637B51f3D9ad5324e98c";
 export const contractABI = abi.abi;
